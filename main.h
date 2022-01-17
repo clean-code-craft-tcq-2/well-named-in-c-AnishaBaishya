9ca5525fd7a7df4395a65a1f443a1d75e8323357
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-01-17 21:37:27 +0530

    Update main.h

--- main.h
+++ main.h
@@ -3,9 +3,11 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+
+#define  MAX_COLORPAIR_NAME_CHARS  16
+
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
-} ColorPair;
- 
-extern ColorPair ; 
+} ColorPair; 
