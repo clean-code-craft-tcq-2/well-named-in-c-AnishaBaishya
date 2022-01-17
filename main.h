5515e2b3e23ac860a9f44cad4507ed70ad0c914e
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-01-17 21:18:44 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#define MAIN_H  
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+ 
+extern ColorPair ; 
