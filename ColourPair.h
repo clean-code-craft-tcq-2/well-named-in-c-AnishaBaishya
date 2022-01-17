cbf7fa3e085cbdc0787cf8c9f0e5eaffe7b53e60
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-01-17 21:41:18 +0530

    Create ColourPair.h

---
+++ ColourPair.h
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/* function prototype */
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
